734df35adb677532e114daf85f274b8414e933d0
Francois Polo <andasan@gmail.com>
2024-03-11 22:55:58 -0700

    :beer: formatting test

--- src/app/_test/orders.test.ts
+++ src/app/_test/orders.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from "vitest";
 import { orders } from "@/app/_data/orders";
+import { describe, expect, it } from "vitest";
 
 describe("orders data structure", () => {
   it("should have a list of orders with specific properties", () => {
